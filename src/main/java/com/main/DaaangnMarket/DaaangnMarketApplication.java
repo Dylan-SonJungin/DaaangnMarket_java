e5980f41f1b7e46af1f68f98a604596547559549
Jungin <jungin.dylan@gmail.com>
2022-12-21 19:03:39 +0900

    Initial commit

---
+++ src/main/java/com/main/DaaangnMarket/DaaangnMarketApplication.java
@@ -0,0 +1,13 @@
+package com.main.DaaangnMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaaangnMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaaangnMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/main/DaaangnMarket/DaaangnMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.DaaangnMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaaangnMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
